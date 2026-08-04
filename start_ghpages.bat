248ec6583506bad929f7844952699bdc51b376af
徐承佑 <llm0968@icloud.com>
2026-08-04 08:38:51 +0800

    feat: 新增指向 GitHub Pages 的一鍵啟動 BAT 腳本

---
+++ start_ghpages.bat
@@ -0,0 +1,9 @@
+@echo off
+chcp 65001 >nul
+title 語音轉錄工具 - GitHub Pages 一鍵啟動 (Microsoft Edge)
+
+echo 正在開啟 GitHub Pages 語音轉錄線上版並啟動語音轉錄...
+
+start msedge "https://a630050.github.io/edgesrt/?autostart=true"
+
+exit
